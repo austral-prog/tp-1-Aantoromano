aabe9f17b7eb9d4fa9b7d7ae55b561a595efd010
Aantoromano <aromano3@mail.austral.edu.ar>
2025-03-21 11:05:29 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,10 @@
 def math():
     a = 57
     b = 7
+    print (a+b)  #suma
+    print (a-b)  #resta
+    print (a*b)  #el producto
+    print ((a+b)/2)  #el promedio 
+    print (a//b)  #el cociente entero
+    print (a%b)  #resto de la division entera
+    print (a/b)  #valor real division
